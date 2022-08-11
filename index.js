cd3a35e72e5fad2209706a39e5e2854bded402ed
Vladyslav09 <kvitka0990@gmail.com>
2022-08-11 08:45:19 +0200

    task

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello you");
+let arg1 = Number(prompt('Fill in first number'));
+let arg2 = Number(prompt('Fill in second number'));
+
+
+function get(num1, num2){
+    let result;
+    if(num1 > num2){
+        return num1;
+    } else{
+        return num2;
+    }
+}
+let result = get(arg1, arg2);
+console.log(result);
+
+
+
+function average(num1, num2){
+    let avrg = (num1 + num2) / 2;
+    return avrg;
+}
+
+let res2 = average(arg1, arg2);
+console.log(res2);
+
+function iseven(num){
+    // if(num % 2 ===0) {
+    //      return true;
+    // } else{
+    //     return false;
+    // }
+    return num % 2 === 0;
+}
+let res3 = iseven(arg1, arg2);
+console.log(res3);
